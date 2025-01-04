24cdf153c6bf59b08ba1a6c3ada12e5e1f434233
maxfa5 <maksfa04@mail.ru>
2025-01-04 17:07:04 +0500

    add limiter respons

--- src/go.mod
+++ src/go.mod
@@ -14,4 +14,5 @@ require (
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/time v0.8.0 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -5,11 +5,16 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"src/api"
 	"src/cache"
+	"strings"
+	"sync"
 	"time"
+
+	"golang.org/x/time/rate"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
@@ -17,6 +22,16 @@ import (
 
 	"github.com/redis/go-redis/v9"
 )
+
+// Create a struct to hold each client's rate limiter
+type Client struct {
+	limiter *rate.Limiter
+}
+
+// In-memory storage for clients
+var clients = make(map[string]*Client)
+var mu sync.Mutex
+var COUNT_RESP_PRE_MIN = 5
 
 func main() {
 	logger := api.Logger_init()
@@ -42,6 +57,8 @@ func main() {
 	defer redisClient.Close()
 
 	r := mux.NewRouter()
+	r.Use(rateLimitingMiddleware(logger))
+
 	r.HandleFunc("/weather/{city}", func(w http.ResponseWriter, r *http.Request) {
 		handleWeatherRequest(w, r, redisClient, param, logger)
 	})
@@ -53,8 +70,23 @@ func main() {
 
 	logger.Infof("Starting server on port: %s", port)
 	http.ListenAndServe(":"+port, r)
+}
 
-	GetWeather(ctx, redisClient, param, api.Init_url(param, logger), logger)
+func rateLimitingMiddleware(logger *logrus.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ip := getClientIP(r)
+			limiter := getClientLimiter(ip, logger)
+
+			if !limiter.Allow() {
+				logger.Warnf("Rate limit exceeded for ip: %s", ip)
+				http.Error(w, "Too Many Requests", http.StatusTooManyRequests)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
 }
 
 func checkCache(ctx context.Context, redisClient *redis.Client, redisKey string, key string, l *logrus.Logger) ([]byte, bool, error) {
@@ -129,4 +161,35 @@ func GetWeather(ctx context.Context, redisClient *redis.Client, param api.Parame
 		return nil, err
 	}
 	return v, nil
+}
+
+func getClientIP(r *http.Request) string {
+	if forwardedFor := r.Header.Get("X-Forwarded-For"); forwardedFor != "" {
+		return strings.Split(forwardedFor, ",")[0]
+	}
+
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return r.RemoteAddr
+	}
+
+	return ip
+}
+
+//getClientLimiter: Get a client's rate limiter or create one if it doesn't exist
+func getClientLimiter(ip string, logger *logrus.Logger) *rate.Limiter {
+	mu.Lock()
+	defer mu.Unlock()
+
+	// If the client already exists, return the existing limiter
+	if client, exists := clients[ip]; exists {
+		return client.limiter
+	}
+
+	// Create a new limiter with 5 requests per minute
+	rateLimit := rate.Every(time.Minute)
+	limiter := rate.NewLimiter(rateLimit, COUNT_RESP_PRE_MIN)
+	logger.Printf("Creating a new limiter for ip: %s, with rateLimit: %v", ip, rateLimit)
+	clients[ip] = &Client{limiter: limiter}
+	return limiter
 }
